05dd9b265c2a56fcae0117c014135f5a5938a1de
mickeias2812 <mickeiaslucena@gmail.com>
2025-09-19 10:52:25 -0300

    colocar sql

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 
 app = Flask(__name__)
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/cadastro_usuario')
+def cadastrar():
+    return render_template('cadastro_usuario.html')
+
+@app.route('/login')
+def logar():
+    return render_template('login.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
